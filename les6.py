82e75cac534b3628131e170cf12ab93296bfd85a
Fahmida024 <158718553+Fahmida024@users.noreply.github.com>
2025-09-27 09:27:00 +0200

    Add files via upload

---
+++ les6.py
@@ -0,0 +1,6 @@
+#step1, make your key value as the second item of the list
+#step3, compare the key with all the values before it
+#step4, shift all the values>key, to the right
+#step5, insert the key in the right position
+#step5, make the next element of the original list, the key
+#step6, repeat step 3,4,5 untill ever element in the list is scanned
