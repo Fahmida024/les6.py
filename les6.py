16daf268511395af26868b3af0c16d9db754ec6c
Fahmida024 <158718553+Fahmida024@users.noreply.github.com>
2025-10-04 09:27:40 +0200

    les6.py

--- les6.py
+++ les6.py
@@ -4,3 +4,15 @@
 #step5, insert the key in the right position
 #step5, make the next element of the original list, the key
 #step6, repeat step 3,4,5 untill ever element in the list is scanned
+
+list=[3,6,4,7,1]
+for i in range(1,len(list)):
+    key=list[i]
+    j=i-1
+    while key>list[j] and j>=0:
+        list[j+1]=list[j]
+        j-=1
+    list[j+1]=key
+    print(key)
+    print(list)
+print (list)
